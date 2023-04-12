133559c41f4ab4192e2f19813f0e5593bfcbfba8
Pruthvi-Parade <parade.pruthvi@gmail.com>
2023-04-12 23:49:38 +0530

    Done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,62 @@
-import './App.css';
-import TodoForm from './Components/TodoForm';
+import './App.css'
+import { useEffect, useState } from "react";
+import ToDo from './Components/Todo';
+import { addToDo, getAllToDo, updateToDo, deleteToDo } from "./Utils/HandleApi";
 
 function App() {
+
+  const [toDo, setToDo] = useState([])
+  const [text, setText] = useState("")
+  const [isUpdating, setIsUpdating] = useState(false)
+  const [toDoId, setToDoId] = useState("")
+
+  useEffect(() => {
+    getAllToDo(setToDo)
+  }, [])
+
+  const updateMode = (_id, text) => {
+    setIsUpdating(true)
+    setText(text)
+    setToDoId(_id)
+  }
+
   return (
-    <div className="to-do-app">
-      <TodoForm/>
+    <div className="App">
+
+      <div className="container">
+
+        <h1>ToDo App</h1>
+
+        <div className="top">
+          <input
+            type="text"
+            placeholder="Add ToDos..."
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+
+          <div
+            className="add"
+            onClick={isUpdating ?
+              () => updateToDo(toDoId, text, setToDo, setText, setIsUpdating)
+              : () => addToDo(text, setText, setToDo)}>
+            {isUpdating ? "Update" : "Add"}
+          </div>
+
+        </div>
+
+        <div className="list">
+
+          {toDo.map((item) => <ToDo 
+          key={item._id} 
+          text={item.text}
+          updateMode = {() => updateMode(item._id, item.text)}
+          deleteToDo = {() => deleteToDo(item._id, setToDo)} />)}
+
+        </div>
+
+      </div>
+
     </div>
   );
 }
