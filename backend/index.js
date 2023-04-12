8176af38812eddede1e893e0860c42d79c8356f6
Pruthvi-Parade <parade.pruthvi@gmail.com>
2023-04-12 11:13:25 +0530

    Backend done

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,27 @@
-console.log("Inside index");
+const express = require("express");
+
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const cors = require("cors");
+
+const routes = require("./routes/TodoRoutes");
+
+const app = express();
+const PORT = process.env.PORT | 5000;
+
+app.use(express.json());
+app.use(cors());
+
+mongoose
+    .connect(process.env.MONGOURL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log("Mongodb Connected..."))
+    .catch((err) => console.error(err));
+
+// Routes
+app.use(routes);
+
+app.listen(PORT, () => console.log("Server running on port " + PORT));
